9f3d6180f9ae35ade390c1e1595c32703c4d7592
Vatsalj17 <vatsaljaiswal17@gmail.com>
2025-06-17 19:58:43 +0530

    added missing file

---
+++ src/shell.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include "../include/shell.h"
+#include "../include/utils.h"
+#include "../include/parser.h"
+#include "../include/colors.h"
+
+char **read_line() {
+	char **command;
+	char *input;
+	char *username = vsh_get_username();
+	char *home = vsh_get_homedir();
+	char hostname[256];
+	gethostname(hostname, sizeof(hostname));
+	char *path = vsh_get_path(home);
+	if (path == NULL) {
+		perror("path");
+        free(path);
+		return NULL;
+	}
+	size_t shell_size = strlen(username) + strlen(hostname) + strlen(path) + 100;
+	char *shell = (char *)malloc(shell_size);
+	snprintf(shell, shell_size, BHGRN"%s"BHRED"@%s"reset":["BLU"%s"reset"]$ ", username, hostname, path);
+	input = readline(shell);
+	if (input == NULL) {
+		printf("Exiting shell...\n");
+		free(path);
+		free(shell);
+		free(input);
+		exit(0);
+	}
+	command = tokenize_input(input);
+	free(input);
+	if (command[0] == NULL) {
+		free(path);
+		free(shell);
+		free(command);
+		return NULL;
+	}
+	free(path);
+	free(shell);
+	return command;
+}
+
